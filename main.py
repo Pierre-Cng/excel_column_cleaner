a105d4c9715f9c2fedb775e7c7df84454accfd9d
Pierre Congiu <pierre.congiu@estaca.eu>
2023-11-29 02:45:17 -0500

    version with 1 ref file

--- main.py
+++ main.py
@@ -1,130 +1,90 @@
-'''
-Coder: Cristina M*********
-Company: Qo***
-Date: 11/13/23
-Purpose: normalize vendors name in an excel file, based on a reference excel file.
-'''
 import pandas as pd 
 
-class df_excel_file():
-    def __init__(self, dict):
-        parameters = dict
-        df = self.dict_to_dataframe(dict)
-    
-    def dict_to_dataframe(self, dict):
-        '''
-        Return a dataftrame made from excel data referenced in dict.
-        '''
-        return pd.read_excel(dict['file_name'],sheet_name=dict['sheet_name'])
-    
+class excel_file():
+    '''
+    Class to build object containing excel file data into pandas dataframe
+    '''
+    def __init__(self, file_name, sheet_name, column):
+        self.file_name = file_name
+        self.sheet_name = sheet_name
+        self.column = column
+        self.df = pd.read_excel(file_name,sheet_name= sheet_name).astype(str)
 
-class normalizer():
-    def __init__(self, dict_target, dict_ref_address, dict_ref_zipcode):
-        target = df_excel_file(dict_target)
-        ref_address = df_excel_file(dict_ref_address)
-        ref_zipcode = df_excel_file(dict_ref_zipcode)
-    
-    def find_in_column(self, string, df_ref, column, trunc_len = 3):
+def find_in_column(string, column, trunc_len = 3):
         '''
-        Return the index position of a string in a dataframe column.
-        The string can be truncated to do a pattern research and increase the 
-        chance to find the term.
+        Find index of a (truncated) string in a given column
         '''
-        string = string[:trunc_len]
-        is_in_column = df_ref[column].str.contains(string, case=False)
+        if isinstance(string, str) is False:
+            string = str(string)
+        if trunc_len != -1:
+            string = string[:trunc_len]
+        is_in_column = column.str.contains(string, case=False)
         index = is_in_column[is_in_column[:] == True].index
         return index
-    
-    def check_correspondance(self, row, column):
-        string  = self.target.df.loc[row, self.target.parameters[column]]
-        for ref in [self.ref_address, self.ref_zipcode]:
-            # Get the index of the string in the reference files 
-            index = self.find_in_column(string, ref.df, ref.parameters[column])
-            if len(index.values) > 0:
-                
 
-        index_ref_zip = self.find_in_column(string, self.df_ref_zipcode, self.ref_zipcode['column_name'])
-
-    # If company name is present in ref_address:
-    if len(index_ref_add.values) > 0:
-        df_target.loc[row, target['column_name']] = df_ref_address.loc[index_ref_add.values[0], ref_address['column_name']]
-
-    # Else if company name is present in ref_zipcode:
-    elif len(index_ref_zip.values) > 0:
-        df_target.loc[row, target['column_name']] = df_ref_zipcode.loc[index_ref_zip.values[0], ref_zipcode['column_name']]
-
-    # Else it is considered as failed and logged in the failed_line_list:
-    else: 
-        failed_line_list.append(df_target.loc[row].values)
-
- 
-
-
-
-def normalize_name(target, ref_address, ref_zipcode):
+def find_in_column_recurssive(string, column):
     '''
-    Function to normalize company name using address reference and zipcode reference.
-    If the name is not contained in any of the reference file, the name row is logged 
-    into 'failed_line_list'.
+    Use of the function find_in_column recurssively until only one index remains
     '''
-    # Declaration of dataframes
-    df_target, df_ref_address, df_ref_zipcode = dict_to_dataframe(target), dict_to_dataframe(ref_address), dict_to_dataframe(ref_zipcode)
-        
-    # Declaration of the list that will contained failed row
-    failed_line_list = []
-    
-    # For each row of the target data:
-    for row in range(len(df_target)):
+    index = find_in_column(string, column)
+    term = 0
+    while len(index) > 1:
+         index = find_in_column(string.split()[term], column, -1)
+         term +=1 
+    return index 
 
-        
-
-    return df_target, failed_line_list
-
-
-
-
-
-def list_to_txt_file(mylist):
+def column_parser(row, target, ref):
     '''
-    Function to log the element of mylist that contains the 
-    row of the df_target file that failed to be normalized
+    Parse column of ref dataframe and execute the function find_in_column_recurssive until it find a matching index or parsed all data
     '''
-    with open("failed_line_output.txt", 'w') as file:
-        for item in mylist:
-            file.write(str(item) + '\n')
+    index = []
+    for key in ref.column.keys():
+        if len(index) == 0:
+            string = target.df.loc[row, target.column[key]]
+            if string != 'nan':
+                index = find_in_column_recurssive(string, ref.df[ref.column[key]])
+        else:
+            return index.values[0]
+    try: 
+        return index.values[0]
+    except:
+        return -1 
+
+def failed_list_to_txt(failed_list):
+    '''
+    Write list of failed line into failed_list.txt file
+    '''
+    with open("failed_list.txt", 'w') as file:
+        for item in failed_list:
+            file.write(str(item) + '\n\n')
         file.close()
 
-
 if __name__ == "__main__":
-    # Parameters to define by user:
-    target = {'file_name' :  'target.xlsx',
-                   'sheet_name' : 'Sheet1',
-                   'column_name' : 'Name:', 
-                   'column_attibute_1' : 'Address:', 
-                   'column_attibute_2' : 'Zipcode:'}
-      
-    ref_address = {'file_name' : 'ref_address.xlsx',
-                        'sheet_name' : 'Sheet1',
-                        'column_name' : 'Name:',
-                        'column_attribute' : 'Address:'}
-    
-    ref_zipcode = {'file_name' : 'ref_zipcode.xlsx',
-                        'sheet_name' : 'Sheet1',
-                        'column_name' : 'Name:',
-                        'column_attibute' : 'Zipcode:'}
-    
-    # Normalizing names
-    df_output, failed_line_list = normalize_name(target_file, ref_address_file, ref_zipcode_file)
-    
-    # Save the failed_line_list log in a txt file named failed_line_output.txt
-    list_to_txt_file(failed_line_list)
+    '''
+    Main code, will be executed if the code is run as a main and not as a library
+    '''
 
-    # Save the normalized dataframe into a new excel file named output.xlsx
-    df_output.to_excel("output.xlsx", index=False)
+    ###### This is the info you need to modify for your own usage #####
+    ###### Syntax:
+     #------=-----------('your file name.xlsx', 'the sheet name in excel',{'name' : 'column name for Name in excel', 'address' : 'column name for Address in excel', 'zipcode' : 'column name for Zipcode in excel'})
+    # target is the file you want to normalize 
+    target = excel_file('target.xlsx', 'Sheet1', {'name' : 'Name:', 'address' : 'Address:', 'zipcode' : 'Zipcode:'})
+    # ref_file is your reference file 
+    ref_file = excel_file('ref_file.xlsx', 'Sheet1', {'name' : 'Name:', 'address' : 'Address:', 'zipcode' : 'Zipcode:'})
+    
+    failed_list=[]
 
-
-    
-    
-            
-    
+    for row in range(len(target.df)):       
+        index = column_parser(row, target, ref_file)
+        if index != -1:   
+            target.df.loc[row, target.column['name']] = ref_file.df.loc[index, ref_file.column['name']] 
+            target.df.loc[row, target.column['address']] = ref_file.df.loc[index, ref_file.column['address']] 
+            target.df.loc[row, target.column['zipcode']] = ref_file.df.loc[index, ref_file.column['zipcode']] 
+            print(target.df.loc[row, target.column['name']], '- Index:', index)
+        else:
+            print('FAILED', 'Name:', target.df.loc[row, target.column['name']])
+            failed_list.append(target.df.loc[row])
+        
+    failed_list_to_txt(failed_list)
+    target.df.to_excel("output.xlsx", index=False)
     
